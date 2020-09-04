b1cf8ad57a70961bf479195489cc20742621eddd
afrolambo <stephennoahgalvan@.com>
2020-09-04 13:31:22 -0400

    start backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :tasks
+  namespace :api do 
+    namespace :v1 do 
+      resources :users
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end 
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
